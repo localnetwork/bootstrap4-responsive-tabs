0579e8681d380d60f944eefc19dd3fe7aef835e4
Dion <diomenike@gmail.com>
2020-03-25 19:23:25 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# bootstrap4-responsive-tabs
+
+
+This library will make the bootstrap nav tabs as dropdown in below 767px screensize.
---
+++ dist/js/responsive-tabs.js
@@ -0,0 +1,20 @@
+// $.fn.responsiveTabs = function() {
+//     this.addClass("responsive-tabs"), this.append($('<span class="drop-arrow"></span>')), this.on("click", "li.active > a, span.fa", function() {
+//         this.toggleClass("open")
+//     }.bind(this)), this.on("click", "li:not(.active) > a", function() {
+//         this.removeClass("open")
+//     }.bind(this))
+// }
+
+$.fn.responsiveTabs = function() {
+	this.addClass('responsive-tabs'),
+	this.append($('<span class="dropdown-arrow"></span>')),
+
+
+
+	this.on("click", "li > a.active", function (){
+		this.toggleClass('open');
+	}.bind(this)), this.on("click", "li > a:not(.active)", function() {
+        this.removeClass("open")
+    }.bind(this)); 
+}  
