96059d05f04349f2f3a26a673303ce88941b7d54
Dion <diomenike@gmail.com>
2020-03-26 11:45:23 +0800

    fixes readme, js file

--- README.md
+++ README.md
@@ -7,5 +7,14 @@
 
 This library will make the bootstrap nav tabs as dropdown in below 768px screensize. Demo here: <a href="https://bootstrap4-responsive-tabs.herokuapp.com/">https://bootstrap4-responsive-tabs.herokuapp.com/</a>
 
+
+<h3>Usage</h3>
+
+<pre><code>$(function() {
+	$('.your-custom-navtabs').responsiveTabs();
+});
+</code></pre>
+
+
 Check the home.html file for your reference.
 
--- dist/js/responsive-tabs.js
+++ dist/js/responsive-tabs.js
@@ -2,15 +2,15 @@
   ** http://github.com/localnetwork/
   **/
 
-$.fn.responsiveTabs = function() {
+(function ($){
+	$.fn.responsiveTabs = function() {
 	this.addClass('responsive-tabs'),
 	this.append($('<span class="dropdown-arrow"></span>')),
 
-
-
 	this.on("click", "li > a.active, span.dropdown-arrow", function (){
-		this.toggleClass('open');
-	}.bind(this)), this.on("click", "li > a:not(.active)", function() {
-        this.removeClass("open")
-    }.bind(this)); 
-}
+			this.toggleClass('open');
+		}.bind(this)), this.on("click", "li > a:not(.active)", function() {
+	        this.removeClass("open")
+	    }.bind(this)); 
+	}
+})(jQuery);
