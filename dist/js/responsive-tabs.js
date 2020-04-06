8e3d28c00da716d580c0eb62af136e0728d9bd8f
Local Network <diomenike@gmail.com>
2020-04-06 18:28:05 +0800

    Update responsive-tabs.js

--- dist/js/responsive-tabs.js
+++ dist/js/responsive-tabs.js
@@ -1,21 +1,6 @@
 /**
   ** http://github.com/localnetwork/
   **/
-<<<<<<< HEAD
-
-
-
-
-// $.fn.responsiveTabs = function() {
-//     this.addClass("responsive-tabs"), this.append($('<span class="drop-arrow"></span>')), this.on("click", "li.active > a, span.fa", function() {
-//         this.toggleClass("open")
-//     }.bind(this)), this.on("click", "li:not(.active) > a", function() {
-//         this.removeClass("open")
-//     }.bind(this))
-// }
-=======
->>>>>>> b14024cb6b6e035b522dcaa892010b9845d6d939
-
 (function ($){
 	$.fn.responsiveTabs = function() {
 	this.addClass('responsive-tabs'),
